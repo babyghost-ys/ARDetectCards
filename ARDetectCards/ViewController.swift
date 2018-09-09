e466feb1cab9c4347e5bfd43f98f9cff9d2bc6ba
Peter Leung <ys.leung.uk@gmail.com>
2018-09-09 17:37:46 +0800

    Added automatic lightning detection for the AR scene view.

--- ARDetectCards/ViewController.swift
+++ ARDetectCards/ViewController.swift
@@ -20,24 +20,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        // Update lightning for better detection
+        sceneView.automaticallyUpdatesLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
